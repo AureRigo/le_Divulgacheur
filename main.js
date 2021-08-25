e556b521e7d296efe5deee1867c0d14c1e60c475
AureRigo <aurelienrigolet3@gmail.com>
2021-08-25 15:23:59 +0200

    My first JS version of my marvelous film is now ready

---
+++ main.js
@@ -0,0 +1,6 @@
+let spoil = document.getElementById("spoil");
+let write = document.getElementById("write");
+
+spoil.onclick = function(){
+    write.innerHTML= "Le chef des rebels est la femme du président Koudjo Sangawe Bamba qui va être démocratiquement élu à 84,4% des voies.";
+}
